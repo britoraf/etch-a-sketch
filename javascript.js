8207b6e74fe35396e5eaf794c0773a28ea68a94e
britoraf <rafaelbrito__@outlook.com>
2025-01-30 23:54:26 +0000

    Creation of 256 divs with for loop

--- javascript.js
+++ javascript.js
@@ -2,17 +2,18 @@
 const container = document.querySelector("#container");
 container.style.backgroundColor = "red";
 container.style.border = "2px solid black";
-container.style.padding = "20px";
-container.style.display = "flex";
+container.style.padding = "10px";
+container.style.display = "inline-flex";
+container.style.justifyContent = "center";
 
+// Divs creation //
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement("div");
+    div.style.padding = "20px";
+    div.style.backgroundColor = "yellow";
+    div.style.border = "1px solid black";
+    container.appendChild(div);
 
-// Creation of 256 divs (16 x 16) //
-const div1 = document.createElement("div");
-div1.style.backgroundColor = "black";
-div1.style.padding = "20px";
-container.appendChild(div1);
-
-const div2 = document.createElement("div");
-div2.style.backgroundColor = "yellow";
-div2.style.padding = "20px";
-container.appendChild(div2);
+  }
+};
